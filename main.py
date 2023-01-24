e74e234486ef1379b81378858e0fc473fcd2f385
vinlee <vinlee1030@gmail.com>
2023-01-24 19:33:21 +0800

    Menu nav test

--- main.py
+++ main.py
@@ -4,5 +4,21 @@ from streamlit_option_menu import option_menu
 # 1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
 EXAMPLE_NO = 1
 
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Main Menu", #None
+        options=["Home","Projects","Contact"],
+        icons=["house","book","envelope"],
+        menu_icon="cast",
+        default_index=0,
+        orientation="vertical" #optional
 
-st.title("H")
+    )
+#st.title("My App")
+
+if selected == "Home":
+    st.title(f"*{selected}")
+if selected == "Projects":
+    st.title(f"*{selected}")
+if selected == "Contact":
+    st.title(f"*{selected}")
