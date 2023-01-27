eec89b9e3c23c68d1d406650f7272a3a9c1de833
vinlee <vinlee1030@gmail.com>
2023-01-27 19:03:21 +0800

    #DETA_KEY = os.getenv("DETA_KEY") DETA_KEY = st.secrets["DETA_KEY"]

--- database.py
+++ database.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 # Load the environment variables
 load_dotenv(".env")
-DETA_KEY = os.getenv("DETA_KEY")
-#DETA_KEY = st.secrets["DETA_KEY"]
+#DETA_KEY = os.getenv("DETA_KEY")
+DETA_KEY = st.secrets["DETA_KEY"]
 
 # Initialize with a project key
 deta = Deta(DETA_KEY)
