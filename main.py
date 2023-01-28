81f67fd0662c882f08a0ff062e7cc59e9f976026
vinlee <vinlee1030@gmail.com>
2023-01-28 17:20:18 +0800

    +To Do List
    Add to do list

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 with st.sidebar:
     selected = option_menu(
         menu_title="Main Menu", #None
-        options=["Home","My Notes","Income&Expense Tracker"], #Contact
+        options=["Home","My Notes","Income&Expense Tracker","To Do List"], #Contact
         icons=["house","book","coin"], #envelope
         menu_icon="cast",
         default_index=0,
@@ -65,6 +65,8 @@ if selected == "Home":
 if selected == "My Notes":
     st.title(f"*{selected}")
 if selected == "Income&Expense Tracker":
+    st.title(f"*{selected}")
+if selected == "To Do List":
     st.title(f"*{selected}")
 #-----------My Notes Tab------------------------
 subjects = ['Macroeconomics','Engineering Math','Mandarin']
@@ -219,7 +221,113 @@ if selected == 'Income&Expense Tracker':
                 fig.update_layout(margin=dict(l=0, r=0, t=5, b=5))
                 st.plotly_chart(fig, use_container_width=True)
 
+#----------------To Do List--------------------------------------------
+from db_fxns import *
+import streamlit.components.v1 as stc
+# Data Viz Pkgs
+import plotly.express as px
+if selected == 'To Do List':
+    menu = ["Create", "Read", "Update", "Delete", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
+    create_table()
 
+    if choice == "Create":
+        st.subheader("Add Item")
+        col1, col2 = st.columns(2)
+
+        with col1:
+            task = st.text_area("Task To Do")
+
+        with col2:
+            task_status = st.selectbox("Status", ["ToDo", "Doing", "Done"])
+            task_due_date = st.date_input("Due Date")
+
+        if st.button("Add Task"):
+            add_data(task, task_status, task_due_date)
+            st.success("Added ::{} ::To Task".format(task))
+
+
+    elif choice == "Read":
+        # st.subheader("View Items")
+        with st.expander("View All"):
+            result = view_all_data()
+            # st.write(result)
+            clean_df = pd.DataFrame(result, columns=["Task", "Status", "Date"])
+            st.dataframe(clean_df)
+
+        with st.expander("Task Status"):
+            task_df = clean_df['Status'].value_counts().to_frame()
+            # st.dataframe(task_df)
+            task_df = task_df.reset_index()
+            st.dataframe(task_df)
+
+            p1 = px.pie(task_df, names='index', values='Status')
+            st.plotly_chart(p1, use_container_width=True)
+
+
+    elif choice == "Update":
+        st.subheader("Edit Items")
+        with st.expander("Current Data"):
+            result = view_all_data()
+            # st.write(result)
+            clean_df = pd.DataFrame(result, columns=["Task", "Status", "Date"])
+            st.dataframe(clean_df)
+
+        list_of_tasks = [i[0] for i in view_all_task_names()]
+        selected_task = st.selectbox("Task", list_of_tasks)
+        task_result = get_task(selected_task)
+        # st.write(task_result)
+
+        if task_result:
+            task = task_result[0][0]
+            task_status = task_result[0][1]
+            task_due_date = task_result[0][2]
+
+            col1, col2 = st.columns(2)
+
+            with col1:
+                new_task = st.text_area("Task To Do", task)
+
+            with col2:
+                new_task_status = st.selectbox(task_status, ["ToDo", "Doing", "Done"])
+                new_task_due_date = st.date_input(task_due_date)
+
+            if st.button("Update Task"):
+                edit_task_data(new_task, new_task_status, new_task_due_date, task, task_status, task_due_date)
+                st.success("Updated ::{} ::To {}".format(task, new_task))
+
+            with st.expander("View Updated Data"):
+                result = view_all_data()
+                # st.write(result)
+                clean_df = pd.DataFrame(result, columns=["Task", "Status", "Date"])
+                st.dataframe(clean_df)
+
+
+    elif choice == "Delete":
+        st.subheader("Delete")
+        with st.expander("View Data"):
+            result = view_all_data()
+            # st.write(result)
+            clean_df = pd.DataFrame(result, columns=["Task", "Status", "Date"])
+            st.dataframe(clean_df)
+
+        unique_list = [i[0] for i in view_all_task_names()]
+        delete_by_task_name = st.selectbox("Select Task", unique_list)
+        if st.button("Delete"):
+            delete_data(delete_by_task_name)
+            st.warning("Deleted: '{}'".format(delete_by_task_name))
+
+        with st.expander("Updated Data"):
+            result = view_all_data()
+            # st.write(result)
+            clean_df = pd.DataFrame(result, columns=["Task", "Status", "Date"])
+            st.dataframe(clean_df)
+
+    else:
+        st.subheader("About ToDo List App")
+        st.info("Built with Streamlit")
+        st.info("Do your tasks faithfully")
+        st.text("Work hard Play hard")
 
 
 
